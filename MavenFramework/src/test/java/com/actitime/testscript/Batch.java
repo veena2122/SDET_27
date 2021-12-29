1f711b8a66f5b934cecd40bd7b5ea66efd029b92
veena <veena@DESKTOP-NJBQ8IK>
2021-12-29 15:43:24 +0530

    new1

---
+++ MavenFramework/src/test/java/com/actitime/testscript/Batch.java
@@ -0,0 +1,18 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Batch {
+		@Test
+		public void testCreateCustomer() {
+			Reporter.log("CreateCustomer",true);
+
+			}
+		public void testCreateOrg() {
+			Reporter.log("CreateOrg");
+		}
+		
+}
+
+
---
+++ MavenFramework/src/test/java/com/actitime/testscript/CreateCustomerModule.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomerModule {
+@Test
+public void testCreateCustomer() {
+	Reporter.log("CreateCustomer",true);
+
+	}
+
+}
